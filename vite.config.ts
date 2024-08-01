2ea9221c8af8640afb91568553d1f98bab24bdd0
xxhls <heyq020814@qq.com>
2024-08-01 15:07:16 +0800

    ci: 配置github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/www.quark.cn/',
   css: {
     modules: {
       localsConvention: 'camelCase'
